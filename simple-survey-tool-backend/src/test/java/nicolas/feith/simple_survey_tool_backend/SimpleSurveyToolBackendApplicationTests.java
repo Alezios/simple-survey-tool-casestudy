06e5d7b693df4da94d478fa8176db0ab98e7ac4c
Nicolas Feith <nicolas.feith@gmail.com>
2025-05-08 14:27:38 +0200

    init(backend)

---
+++ simple-survey-tool-backend/src/test/java/nicolas/feith/simple_survey_tool_backend/SimpleSurveyToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolas.feith.simple_survey_tool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSurveyToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
